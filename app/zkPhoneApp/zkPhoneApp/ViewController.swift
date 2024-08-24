34ed840fca532dbaf37c53121833f6a61fbf5ab4
bati668 <bati@hiroshi.in>
2024-08-24 13:49:44 +0900

    root view

--- app/zkPhoneApp/zkPhoneApp/ViewController.swift
+++ app/zkPhoneApp/zkPhoneApp/ViewController.swift
@@ -22,9 +22,29 @@ class ViewController: UIViewController {
         let result = try! groth16Verify(proof: proof.proof.data(using: .utf8)!, inputs: proof.publicSignals.data(using: .utf8)!, verificationKey: v)
         print(result)
         
-        // Do any additional setup after loading the view.
+        
+        let smsButton = UIButton()
+        smsButton.setTitle("電話番号入力画面へ", for: .normal)
+        smsButton.frame = CGRect(x: (view.bounds.size.width - 200) / 2, y: (view.bounds.height - 50) / 2, width: 200, height: 50)
+        smsButton.setTitleColor(.black, for: .normal)
+        smsButton.addTarget(self, action: #selector(smsButtonDidTapped), for: .touchUpInside)
+        view.addSubview(smsButton)
+        
+        let verifyButton = UIButton()
+        verifyButton.setTitle("Proof検証画面へ", for: .normal)
+        verifyButton.frame = CGRect(x: (view.bounds.size.width - 200) / 2, y: smsButton.frame.origin.y + smsButton.frame.height + 15.0, width: 200, height: 50)
+        verifyButton.setTitleColor(.black, for: .normal)
+        verifyButton.addTarget(self, action: #selector(verifyButtonDidTapped), for: .touchUpInside)
+        view.addSubview(verifyButton)
     }
 
+    @objc func smsButtonDidTapped() {
+        
+    }
+    
+    @objc func verifyButtonDidTapped() {
+        
+    }
 
 }
 
