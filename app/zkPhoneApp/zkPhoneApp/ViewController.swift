b97de518d0444e7e81dc5b4126ab0453b56de2a6
bati668 <bati@hiroshi.in>
2024-08-24 13:26:13 +0900

    create io app

---
+++ app/zkPhoneApp/zkPhoneApp/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  zkPhoneApp
+//
+//  Created by Hiroshi Chiba on 2024/08/24.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
